d0cb73692a8109c343c198ecef45bec71198f9e4
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-01-21 12:07:29 -0800

    first commit

---
+++ ContraintsPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContraintsPractice
+//
+//  Created by Gonzalo Birrueta on 1/21/20.
+//  Copyright © 2020 Gonzalo Birrueta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
