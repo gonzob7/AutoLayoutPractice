94c2fede29592eafcdce879cc8b43bcfbf1157a3
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-01-28 11:42:15 -0800

    started scroll view

--- ContraintsPractice/OnboardingViewController.swift
+++ ContraintsPractice/OnboardingViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class OnboardingViewController: UIViewController{
     
     var firstView: UIView!
+    var secondView: UIView!
     var pageControl: UIPageControl!
+    var scrollView: UIScrollView!
     
     override func loadView() {
         super.loadView()
@@ -19,20 +21,47 @@ class OnboardingViewController: UIViewController{
         firstView = UIView(frame: .zero)
         firstView.translatesAutoresizingMaskIntoConstraints = false
         
+        secondView = UIView(frame: .zero)
+        secondView.translatesAutoresizingMaskIntoConstraints = false
+        
         pageControl = UIPageControl(frame: .zero)
         pageControl.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(firstView)
-        self.view.addSubview(pageControl)
+        scrollView = UIScrollView(frame: .zero)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+        self.view.addSubview(scrollView)
+        self.scrollView.addSubview(firstView)
+        self.scrollView.addSubview(secondView)
+        self.scrollView.addSubview(pageControl)
+        
         
         NSLayoutConstraint.activate([
+            scrollView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 3/1),
+            scrollView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1/1),
+            scrollView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            scrollView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            
+            
             firstView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1/1),
             firstView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1/1),
             firstView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             firstView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
             
             pageControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            pageControl.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            
+            
+            secondView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1/1),
+            secondView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1/1),
+            secondView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            secondView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            
+            pageControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             pageControl.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+            
         ])
 
         
@@ -41,6 +70,7 @@ class OnboardingViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         self.firstView.backgroundColor = .purple
+        self.secondView.backgroundColor = .blue
         self.pageControl.numberOfPages = 3
     }
 
