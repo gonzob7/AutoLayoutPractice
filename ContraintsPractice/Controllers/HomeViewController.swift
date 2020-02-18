9f85c94b13bcd27d4890ac36deb1ae87cf3f3092
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-02-17 16:20:25 -0800

    added buttons to home vc

--- ContraintsPractice/Controllers/HomeViewController.swift
+++ ContraintsPractice/Controllers/HomeViewController.swift
@@ -13,15 +13,43 @@ class HomeViewController: UIViewController{
     let stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.spacing = 0
+        stackView.spacing = 30
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.distribution = .fill
+        stackView.distribution = .fillEqually
         return stackView
     }()
     
     let newBox: UIButton = {
-        let newBox = UIButton()
-        return newBox
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("New Box", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(red:0.44, green:0.43, blue:0.98, alpha:1.0)
+        button.titleLabel?.font = UIFont(name: "AvenirNextCondensed-Bold", size: 18)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    let pastBoxes: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Past Boxes", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(red:0.44, green:0.43, blue:0.98, alpha:1.0)
+        button.titleLabel?.font = UIFont(name: "AvenirNextCondensed-Bold", size: 18)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    let profileBox: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Profile", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(red:0.44, green:0.43, blue:0.98, alpha:1.0)
+        button.titleLabel?.font = UIFont(name: "AvenirNextCondensed-Bold", size: 18)
+        button.layer.cornerRadius = 10
+        return button
     }()
     
     override func viewDidLoad() {
@@ -44,8 +72,30 @@ class HomeViewController: UIViewController{
     
     func setStackView(){
         
+        self.view.addSubview(stackView)
+        
+        stackView.widthAnchor.constraint(equalTo: self.view.layoutMarginsGuide.widthAnchor, multiplier: 0.65).isActive = true
+        stackView.heightAnchor.constraint(equalTo: self.view.layoutMarginsGuide.heightAnchor, multiplier: 0.75).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor).isActive = true
         
         
+        stackView.addArrangedSubview(newBox)
+        
+        newBox.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
+        newBox.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        
+        stackView.addArrangedSubview(pastBoxes)
+        
+        pastBoxes.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
+        pastBoxes.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        
+        stackView.addArrangedSubview(profileBox)
+        
+        profileBox.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
+        profileBox.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
     }
 
