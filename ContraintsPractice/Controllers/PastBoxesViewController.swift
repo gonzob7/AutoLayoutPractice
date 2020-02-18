e1ad7e912aca7d4f7a96834946fbcab497b9b3c6
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-02-18 11:19:58 -0800

    created table view, no data yet

--- ContraintsPractice/Controllers/PastBoxesViewController.swift
+++ ContraintsPractice/Controllers/PastBoxesViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class PastBoxesViewController: UIViewController, UITableViewController{
+class PastBoxesViewController: UIViewController{
     
+    let tableView =  UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setViews()
         setUpNavBar()
         setUpTableView()
+    }
+    
+    override func loadView() {
+      super.loadView()
     }
     
     func setViews(){
@@ -29,8 +34,14 @@ class PastBoxesViewController: UIViewController, UITableViewController{
     }
     
     func setUpTableView(){
-        
-        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.backgroundColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.0)
+
     }
     
     
