f7e9d5b2d9752bca33bb1e4588bf289a774bc534
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-02-20 17:37:52 -0800

    created favorite model

---
+++ ContraintsPractice/Models/Favorite.swift
@@ -0,0 +1,15 @@
+//
+//  Favorite.swift
+//  ContraintsPractice
+//
+//  Created by Gonzalo Birrueta on 2/20/20.
+//  Copyright © 2020 Gonzalo Birrueta. All rights reserved.
+//
+
+import UIKit
+
+struct Favorite{
+    var title: String
+    var type: String
+    var image: UIImage
+}
