bf1618af5227d3a76963a3a466dae95c5d1ebd2f
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-03-04 20:06:54 -0800

    added bounce animation when favorite button pushed

--- ContraintsPractice/Views/CustomPastBoxDetailCell.swift
+++ ContraintsPractice/Views/CustomPastBoxDetailCell.swift
@@ -76,6 +76,17 @@ class CustomPastBoxDetailCell: UITableViewCell{
     
     @objc func favTapped(){
         if self.favoriteButton.currentImage == UIImage(named: "unfavorite"){
+            
+            UIView.animate(withDuration: 0.3,
+            animations: {
+                self.favoriteButton.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            },
+            completion: { _ in
+                UIView.animate(withDuration: 0.3) {
+                    self.favoriteButton.transform = CGAffineTransform.identity
+                }
+            })
+            
             self.favoriteButton.setImage(UIImage(named: "favorite"), for: .normal)
         }else{
             self.favoriteButton.setImage(UIImage(named: "unfavorite"), for: .normal)
